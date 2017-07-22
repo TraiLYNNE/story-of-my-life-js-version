375da9e6fe14b5cf4e6d8786c022c80be3638dc4
Trai Compton <tlcompton2546@outlook.com>
2017-07-22 18:36:41 +0000

    EntryModel: Began making JS logic for navigating to between entries by adding event listener to the 'next' link on the show view

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -10,4 +10,11 @@ $(function () {
 			$('#entryContent').text(resp["content"]);
 		});
 	});
+	
+	$('.js-more').on("click", function() {
+		var nextId = parseInt($('.js-next').attr('data-id')) + 1;
+		$.get("/entries/" + nextId + ".json", function(resp) {
+			// body...
+		});
+	});
 });
