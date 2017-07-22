ddee61f7a31e9b763bf41c2d1a2fb091f5d6182c
Trai Compton <tlcompton2546@outlook.com>
2017-07-22 01:49:17 +0000

    EntryModel: Moved JS logic for new entry from js file to view for now

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,5 +1,13 @@
 $(function(){
     $('form').submit(function(e) {
         e.preventDefault();
+        
+        var values = $(this).serialize();
+        
+        var entry = $.post('/entry', values);
+        
+        entry.done(function(data) {
+            alert('new post created');
+        });
     });
 })
